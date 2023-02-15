dc0dd3e6ee161004f1b2d8ef21729b5f7174face
Alex Kozlov <kozlaleksan2013@gmail.com>
2023-02-15 13:01:55 +0300

    new

--- script.py
+++ script.py
@@ -1,15 +1,31 @@
-money = int(input("Введите сумму средств: "))
-per_cent = {'ТКБ': 5.6,
-            'СКБ': 5.9,
-            'ВТБ': 4.28,
-            'СБЕР': 4.0
-            }
-deposit = list(map(round, [per_cent['ТКБ']*money/100, per_cent['СКБ']*money/100, per_cent['ВТБ']*money/100, per_cent['СБЕР']*money/100]))
-print(deposit)
-max_deposit = max(deposit)
-print("Максимальная сумма, которую вы можете заработать —", max_deposit)
+# счетчики
+count1 = 0
+count2 = 0
+count3 = 0
 
-#print("В ТКБ вы накопите %d за год вклада" % (per_cent['ТКБ']*float(money)))
-#print("В СКБ вы накопите %d за год вклада" % (per_cent['СКБ']*float(money)))
-#print("В ВТБ вы накопите %d за год вклада" % (per_cent['ВТБ']*float(money)))
-#print("В СБЕР вы накопите %d за год вклада" % (per_cent['СБЕР']*float(money)))
+#цена билета в категориях возраста
+sum1 = 0
+sum2 = 990
+sum3 = 1390
+
+ticket = int(input("Сколько билетов Вы желаете приобрести? "))
+
+for a in range(1, ticket + 1):
+    age = int(input(f"Введите возраст посетителя для билета #{a}: "))
+    if age < 18:
+        count1 += 1
+        print(f"Цена за билет: {sum1}")
+    elif 18 <= age <= 25:
+        count2 += 1
+        print(f"Цена за билет: {sum2}")
+    elif age > 25:
+         count3 += 1
+         print(f"Цена за билет: {sum3}")
+
+all_summa = count1*sum1 + count2*sum2 + count3*sum3
+
+if ticket > 3:
+    print("Общая сумма: ", all_summa)
+    print("Сумма с учетом скидки", all_summa - all_summa / 10)
+else:
+    print("Общая сумма: ", all_summa)
